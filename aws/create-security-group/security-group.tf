09ed5c5476b6710ef557ac1099661fbe961d9225
Varsha Aaynure <66114947+VarshaAaynure@users.noreply.github.com>
2024-02-02 18:00:14 +0530

    Create security-group.tf

---
+++ aws/create-security-group/security-group.tf
@@ -0,0 +1,37 @@
+/*
+port 80 -> HTTP
+port 443 -> HTTPS
+port 22 -> SSH
+port 25 -> SMTP
+*/
+
+# creating security group 
+resource "aws_security_group" "my-first-sg" {
+  name        = "my-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
