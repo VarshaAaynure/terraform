f30f40fabdd107e541caa8eea0e591c06d85f32d
Varsha Aaynure <66114947+VarshaAaynure@users.noreply.github.com>
2024-04-04 14:52:25 +0530

    Create main.tf

---
+++ multi-block/main.tf
@@ -0,0 +1,13 @@
+// multi block in single terraform file
+
+output "firstOutputblock" {
+  value = "First Block"
+}
+
+output "secondOutputblock" {
+  value = "Second Block"
+}
+
+output "thirdBlock" {
+  value = "Third Block"
+}
