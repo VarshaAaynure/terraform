d0210b4861e0fd838dff422d171491856dddb16a
Varsha Aaynure <66114947+VarshaAaynure@users.noreply.github.com>
2024-04-05 14:02:14 +0530

    Create main.tf

---
+++ tf-console-cmd/main.tf
@@ -0,0 +1,17 @@
+/* 
+$ terraform fmt
+$ terraform console 
+> var.surname
+> var.name
+> exit
+*/
+variable "name" {
+  default = "varsha"
+}
+
+variable "age" {
+  default = 25
+}
+
+variable "surname" {
+}
